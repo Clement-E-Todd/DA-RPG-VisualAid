32d0172c421a2e4b081196d7b39e8ded2d71e93d
Unknown <clem.e.todd@gmail.com>
2018-09-03 20:03:53 -0400

    Added more placeholder UI

---
+++ Assets/Scripts/CharacterModel/ColorPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPanel : MonoBehaviour
+{
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+    public Image preview;
+
+    public Color GetColor()
+    {
+        return new Color(redSlider.value, greenSlider.value, blueSlider.value);
+    }
+
+    public void OnColorSliderValueChanged()
+    {
+        preview.color = GetColor();
+    }
+}
