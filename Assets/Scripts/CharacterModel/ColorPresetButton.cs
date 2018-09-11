78356d052c22e458e1df36946977ce3d0c47eb63
Unknown <clem.e.todd@gmail.com>
2018-09-10 20:40:04 -0400

    Hair color controls

---
+++ Assets/Scripts/CharacterModel/ColorPresetButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPresetButton : MonoBehaviour
+{
+    public ColorPanel colorPanel;
+
+    public void OnClicked()
+    {
+        Image image = GetComponent<Image>();
+
+        colorPanel.redSlider.value = image.color.r;
+        colorPanel.greenSlider.value = image.color.g;
+        colorPanel.blueSlider.value = image.color.b;
+
+        colorPanel.OnColorSliderValueChanged();
+    }
+}
---
+++ Assets/Scripts/CharacterModel/HeadBodyPart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBodyPart : BodyPart
+{
+    public SpriteRenderer eyeColorSprite;
+
+    public SpriteRenderer hairSprite;
+    public ColorPanel hairColorPanel;
+    public OutfitCategory hairStyles;
+
+    public SpriteRenderer specialSprite;
+    public OutfitCategory specialStyles;
+
+    public void OnHairColorPanelUpdated()
+    {
+        hairSprite.color = new Color(
+            hairColorPanel.redSlider.value,
+            hairColorPanel.greenSlider.value,
+            hairColorPanel.blueSlider.value
+        );
+    }
+}
