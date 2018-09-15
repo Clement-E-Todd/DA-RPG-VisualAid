5c0c6ebc815cfdb58e29b02b2b2db03a1257092f
Unknown <clem.e.todd@gmail.com>
2018-09-15 11:50:23 -0400

    Hooked up horn controls for male qunari

--- Assets/Scripts/CharacterModel/HeadBodyPart.cs
+++ Assets/Scripts/CharacterModel/HeadBodyPart.cs
@@ -3,16 +3,13 @@ using UnityEngine.UI;
 
 public class HeadBodyPart : BodyPart
 {
+    public SpriteRenderer eyeColorSprite;
+    public ColorPanel eyeColorPanel;
+
     public SpriteRenderer hairSprite;
     public ColorPanel hairColorPanel;
     public OutfitCategory hairStyles;
     public Text hairIndexText;
-
-    public SpriteRenderer eyeColorSprite;
-    public ColorPanel eyeColorPanel;
-
-    public SpriteRenderer specialSprite;
-    public OutfitCategory specialStyles;
 
     private Outfit hairStyle
     {
@@ -22,12 +19,37 @@ public class HeadBodyPart : BodyPart
         }
     }
 
+    public SpriteRenderer specialSprite;
+    public ColorPanel specialColorPanel;
+    public OutfitCategory specialStyles;
+    public Text specialIndexText;
+
+    private Outfit specialStyle
+    {
+        get
+        {
+            return specialStyles.selectedIndex >= 0 ? specialStyles.outfits[specialStyles.selectedIndex] : null;
+        }
+    }
+
     protected override void Awake()
     {
         base.Awake();
 
         hairIndexText.text = (hairStyles.selectedIndex + 1).ToString();
         OnHairIndexUpdated();
+
+        specialIndexText.text = (specialStyles.selectedIndex + 1).ToString();
+        OnSpecialIndexUpdated();
+    }
+
+    public void OnEyeColorPanelUpdated()
+    {
+        eyeColorSprite.color = new Color(
+            eyeColorPanel.redSlider.value,
+            eyeColorPanel.greenSlider.value,
+            eyeColorPanel.blueSlider.value
+        );
     }
 
     public void OnHairColorPanelUpdated()
@@ -80,12 +102,53 @@ public class HeadBodyPart : BodyPart
         }
     }
 
-    public void OnEyeColorPanelUpdated()
+    public void OnSpecialColorPanelUpdated()
     {
-        eyeColorSprite.color = new Color(
-            eyeColorPanel.redSlider.value,
-            eyeColorPanel.greenSlider.value,
-            eyeColorPanel.blueSlider.value
+        specialSprite.color = new Color(
+            specialColorPanel.redSlider.value,
+            specialColorPanel.greenSlider.value,
+            specialColorPanel.blueSlider.value
         );
+    }
+
+    public void IncrementSpecialIndex()
+    {
+        specialStyles.selectedIndex += 1;
+        if (specialStyles.selectedIndex >= specialStyles.outfits.Length)
+        {
+            specialStyles.selectedIndex -= specialStyles.outfits.Length + 1;
+        }
+        specialIndexText.text = (specialStyles.selectedIndex + 1).ToString();
+
+        OnSpecialIndexUpdated();
+    }
+
+    public void DecrementSpecialIndex()
+    {
+        specialStyles.selectedIndex -= 1;
+        if (specialStyles.selectedIndex < -1)
+        {
+            specialStyles.selectedIndex += specialStyles.outfits.Length + 1;
+        }
+        specialIndexText.text = (specialStyles.selectedIndex + 1).ToString();
+
+        OnSpecialIndexUpdated();
+    }
+
+    public void OnSpecialIndexUpdated()
+    {
+        if (specialStyles.outfits.Length > 0 && specialStyles.selectedIndex >= 0)
+        {
+            if (specialStyle.sprite == null)
+            {
+                specialStyle.sprite = Resources.Load<Sprite>(specialStyle.spritePath);
+            }
+
+            specialSprite.sprite = specialStyle.sprite;
+        }
+        else
+        {
+            specialSprite.sprite = null;
+        }
     }
 }
