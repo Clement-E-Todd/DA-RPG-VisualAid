6509d4915cc53e6349dddad7a4152077c783f411
Unknown <clem.e.todd@gmail.com>
2018-11-04 22:06:39 -0500

    Started working on props, added a test prop that can be dragged

---
+++ Assets/Scripts/BattleMap/BattleMapProp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMapProp : MonoBehaviour
+{
+    public static BattleMapProp selectedProp = null;
+
+    Vector3 propDragStartPos;
+    Vector3 mouseDragStartPos;
+
+    SpriteRenderer _spriteRenderer;
+    SpriteRenderer spriteRenderer
+    {
+        get
+        {
+            if (!_spriteRenderer)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+
+            return _spriteRenderer;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (spriteRenderer.color.a < 0.25f)
+        {
+            return;
+        }
+
+        selectedProp = this;
+        propDragStartPos = transform.position;
+        mouseDragStartPos = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (selectedProp != this)
+        {
+            return;
+        }
+        transform.position = propDragStartPos + (Input.mousePosition - mouseDragStartPos) / 128f;
+    }
+}
